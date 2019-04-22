bf990e2c47bf7e5426ce07e4f161f5f8cf8146a2
Julio Cesar <julio.rodriguezr1@gmail.com>
2019-04-22 01:55:53 -0300

    seccion 5 almost done: just need to put the green circles controls at the bottom

--- js/app.js
+++ js/app.js
@@ -7,3 +7,23 @@ for (let i = 0; i < cardArray.length; i++) {
 // .classList.add('colorGrey')
 
 // cardArray[0].children[0].children[2].style.display = 'none';
+
+var fraseArray = document.getElementsByClassName('frase');
+
+var authorArray = document.getElementsByClassName('author');
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(function (json) {
+    for (let i = 0; i < fraseArray.length; i++) {
+      fraseArray[i].innerHTML = json[i].body;
+    }
+  });
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => {
+    for (let i = 0; i < authorArray.length; i++) {
+      authorArray[i].innerHTML = json[i].name;
+    }
+  });
