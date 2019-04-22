60f40290e247d8c7988ceb8686303d6b723cbe58
Julio Cesar <julio.rodriguezr1@gmail.com>
2019-04-21 23:44:59 -0300

    mejoras a las secciones de la 1 a la 4..ahora empieza la 5ta

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 /* eslint-disable eol-last */
 var cardArray = document.getElementsByClassName('card');
 for (let i = 0; i < cardArray.length; i++) {
-  cardArray[i].classList.add('animated', 'bounceIn');
+  cardArray[i].classList.add('wow', 'bounceIn');
 }
+
+// .classList.add('colorGrey')
+
+// cardArray[0].children[0].children[2].style.display = 'none';
